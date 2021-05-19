eb6469d7100e3e6f55cf5ba19e6bf47e13fe2014
Dasia Williams <dasiawilliams2016@gmail.com>
2021-05-19 16:19:23 -0400

    Add files via upload

---
+++ crackle.py
@@ -0,0 +1,12 @@
+for number in range(1, 101):
+        if number % 15 == 0:
+                print("CracklePop")
+                continue
+        elif number % 3 == 0:
+                print("Crackle")
+                continue
+        elif number % 5 == 0:
+                print("Pop")
+                continue
+        else:
+                print(number)
